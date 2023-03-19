b346ab23f237fca7723e66d4be3ba8edd90f65c7
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2023-03-19 14:31:43 +0530

    Init ec2-bot

---
+++ ec2-bot/src/handlers/scheduled-event-logger.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+const ec2 = new AWS.EC2({ region: 'us-east-1' });
+
+exports.handler = async (event, context) => {
+  try {
+    const instances = await ec2.describeInstances({ Filters: [{ Name: 'instance-state-name', Values: ['running'] }] }).promise();
+    const instanceIds = instances.Reservations.reduce((acc, reservation) => acc.concat(reservation.Instances.map(instance => instance.InstanceId)), []);
+    if (instanceIds.length) {
+      await ec2.stopInstances({ InstanceIds: instanceIds }).promise();
+      console.log(`Stopped instances: ${instanceIds}`);
+    } else {
+      console.log('No instances to stop.');
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
